e95af8d807304590aa3555f7d522fd1973ef4c77
TijmenH <tijmenhelder@gmail.com>
2017-10-30 21:47:05 +0100

    adding handlebars for template engine & setting it up

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const exphbs  = require('express-handlebars');
+
 const app = express();
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 // Static files directory
 app.use('/public', express.static(__dirname + '/build'));
 
 // Root route
 app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/views/index.html');
+	// res.sendFile(__dirname + '/views/index.html');
+	res.render('employee-list', {test: 'test'})
 });
 
 // Server
