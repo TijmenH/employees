14f16a432be0c1789a964d9c339c4f063d777e01
TijmenH <tijmenhelder@gmail.com>
2017-10-30 22:31:37 +0100

    Adding request to get the employee data

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const request = require('request');
 const app = express();
 
 // Static files directory
 app.use('/public', express.static(__dirname + '/build'));
 
 // Root route
-app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/views/index.html');
+app.get('/', async (req, res) => {
+	// Get data using request package
+	request('http://sys4.open-web.nl/employees.json', (error, response, data) => {
+		console.log(data);
+		res.sendFile(__dirname + '/views/index.html');
+	});
 });
 
 // Server
