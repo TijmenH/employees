932a76127c7c1df20e31b840261ea13bb068c6b6
TijmenH <tijmenhelder@gmail.com>
2017-10-30 23:15:52 +0100

    Removing module to render employee list

--- src/js/index.js
+++ src/js/index.js
@@ -8,14 +8,12 @@ import '../scss/main.scss'; // @NOTE webpack will strip this out
 * @description
 * Module imports
 */
-import Employees from './modules/employees';
+// Nothing yet
 
 /**
 * @description
 * Event listener to load all modules as soon the DOM is loaded
 */
 document.addEventListener('DOMContentLoaded', () => {
-	if (document.querySelector('.employees-page')) {
-		// new Employees();
-	}
+	console.log('js initialized');
 });
